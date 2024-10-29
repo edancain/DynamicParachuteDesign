7c004f4f2071a5b6f73d226fcbb9136aa961967d
edancain <edan.cain@gmail.com>
2024-10-29 17:46:09 +1300

    Updates to try to get the cell graph working

---
+++ my-parachute-calculator/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your config options here
+  reactStrictMode: true,
+  // ... any other configuration options you had
+}
+
+module.exports = nextConfig
